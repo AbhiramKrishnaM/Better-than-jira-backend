3d12cd54b525cd24003b9021b77cea9a3e99fa83
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-27 14:36:31 +0530

    feat: added database configuration

--- db.config.js
+++ db.config.js
@@ -3,3 +3,11 @@ import pg from "pg";
 const Pool = pg.Pool;
 
 console.log(Pool);
+
+export const pool = new Pool({
+  user: process.env.POSTGRE_USER,
+  host: process.env.POSTGRE_HOST,
+  database: process.env.POSTGRE_DATABASE,
+  password: process.env.POSTGRE_PASSWORD,
+  port: process.env.POSTGRE_PORT,
+});
