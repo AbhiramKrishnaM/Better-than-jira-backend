d709604c54669445e2aade398fc79bf5a51edef7
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2022-12-30 12:34:22 +0530

    feat: adde user routes

--- routes/Route.users.js
+++ routes/Route.users.js
@@ -1,1 +1,13 @@
-import pool from "";
+import pool from "../Database/db.config.js";
+import express from "express";
+
+const router = express.Router();
+
+router.get(); // get users
+router.get(); // get user by id
+router.post(); // create new user
+
+router.put(); // update a particular user details
+router.delete(); // delete user
+
+export default router;
